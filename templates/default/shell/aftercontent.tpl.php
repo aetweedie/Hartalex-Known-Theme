5ea4c8ab4c06bab290a01a9af9faa9641cd81d55
Alan Tweedie <aetweedie@gmail.com>
2015-08-12 13:21:28 -0600

    fartscroll will only be called on fridays

--- templates/default/shell/aftercontent.tpl.php
+++ templates/default/shell/aftercontent.tpl.php
@@ -5,5 +5,11 @@
 
 <script src="http://code.onion.com/fartscroll.js"></script>
 <script type="text/javascript">
-  fartscroll(800);
+  var checkDay = function(day) {
+    if (Date.day === 'friday'){
+      fartscroll();
+    } else {
+      return;
+    }
+  }
 </script>
