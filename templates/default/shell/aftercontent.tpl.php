826eb6c5d0b8fc7ae20c7c0d163fd4903824ff12
Alan Tweedie <aetweedie@gmail.com>
2015-08-12 13:26:25 -0600

    mild syntax error use the date method

--- templates/default/shell/aftercontent.tpl.php
+++ templates/default/shell/aftercontent.tpl.php
@@ -6,8 +6,8 @@
 <script src="http://code.onion.com/fartscroll.js"></script>
 <script type="text/javascript">
   var checkDay = function(day) {
-    if (Date.day === 'friday'){
-      fartscroll();
+    if (Date.getDay === 4){
+      fartscroll(800);
     } else {
       return;
     }
